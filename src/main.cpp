a791b8cc2164f2bf4008706ead3a23e1a55cd490
Doyeop Kim <dygames34@gmail.com>
2023-11-02 17:33:45 +0900

    feat: add more VAO

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <vector>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+typedef struct _Model
+{
+    std::vector<float> vertices;
+    uint VBO;
+    uint VAO;
+} Model;
+
+Model* models[] = {
+    new Model(),
+    new Model(),
+};
+
+unsigned int shaderProgram;
 
 void processInput(GLFWwindow *window)
 {
@@ -8,21 +23,18 @@ void processInput(GLFWwindow *window)
         glfwSetWindowShouldClose(window, true);
 }
 
-unsigned int shaderProgram;
-unsigned int VAO, VBO;
-
 void initShader()
 {
-
     int success = false;
     char infoLog[512];
     // shader
-    const char *vertextShaderSource = "#version 330 core\n"
-                                      "layout (location = 0) in vec3 aPos;\n"
-                                      "void main()\n"
-                                      "{\n"
-                                      "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                      "}\0";
+    const char *vertextShaderSource = R"fs(#version 330 core
+                                      layout (location = 0) in vec3 aPos;
+
+                                      void main()
+                                      {
+                                         gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+                                      })fs";
 
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertextShaderSource, NULL);
@@ -32,16 +44,17 @@ void initShader()
     if (!success)
     {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
     }
 
-    const char *fragmentShaderSource = "#version 330 core\n"
-                                       "out vec4 FragColor;\n"
-                                       "\n"
-                                       "void main()\n"
-                                       "{\n"
-                                       "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                                       "}\0";
+    const char *fragmentShaderSource = R"fs(#version 330 core
+                                       out vec4 FragColor;
+
+                                       void main()
+                                       {
+                                          FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+                                       })fs";
 
     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
@@ -50,7 +63,8 @@ void initShader()
     if (!success)
     {
         glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
     }
 
     shaderProgram = glCreateProgram();
@@ -58,34 +72,30 @@ void initShader()
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
+    if (!success)
+    {
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+                  << infoLog << std::endl;
     }
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 }
 
-void initVAO()
+void initModel(Model* model)
 {
-    float vertices[] = 
-    {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.5f,
-        0.0f, 0.5f, 0.0f
-    };
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &model->VAO);
+    glGenBuffers(1, &model->VBO);
 
     // bind vertex array object
-    glBindVertexArray(VAO);
-    
+    glBindVertexArray(model->VAO);
+
     // copy vertices array in a buffer
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, model->VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(model->vertices), &*model->vertices.begin(), GL_STATIC_DRAW);
 
     // set vertex attributes pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -99,11 +109,15 @@ void render()
     glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(shaderProgram);
-    glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    for (size_t i = 0; i < 2; i++)
+    {
+        glBindVertexArray(models[i]->VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+    }
 }
 
-void error(int error_code, const char *description)
+void error(int error_code, const char* description)
 {
     std::cout << description << std::endl;
 }
@@ -115,6 +129,16 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 
 int main()
 {
+    models[0]->vertices = {
+        -0.7f, -0.5f, 0.0f,
+        -0.3f, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f};
+
+    models[1]->vertices = {
+        0.3f, -0.5f, 0.0f,
+        0.7f, -0.5f, 0.0f,
+        0.5f, 0.5f, 0.0f};
+
     if (!glfwInit())
     {
         std::cout << "glfw init error" << std::endl;
@@ -125,12 +149,12 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #ifdef __APPLE__
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
     GLFWwindow *window = glfwCreateWindow(800, 600, "OpenGL Test", NULL, NULL);
     if (window == NULL)
-    {        
+    {
         std::cout << "glfw create window error" << std::endl;
         glfwTerminate();
         return -1;
@@ -141,13 +165,14 @@ int main()
     glfwMakeContextCurrent(window);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {       
+    {
         std::cout << "glfw load gll error" << std::endl;
         return -1;
     }
 
     initShader();
-    initVAO();
+    initModel(models[0]);
+    initModel(models[1]);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -159,8 +184,11 @@ int main()
         glfwPollEvents();
     }
 
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    for (size_t i = 0; i < 2; i++)
+    {
+        glDeleteVertexArrays(1, &models[i]->VAO);
+        glDeleteBuffers(1, &models[i]->VBO);
+    }
     glDeleteProgram(shaderProgram);
 
     glfwTerminate();
